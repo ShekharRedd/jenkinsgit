05355fef8c7d1d9f0a9a000f457037524d63abed
ShekharRedd <115493437+ShekharRedd@users.noreply.github.com>
2023-04-21 22:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,3 @@ for i in range(10):
     print(random.randint(0,10))
 
 
-
-print("  hckhdkjcdj hi sir")
-
-
-
-for i in range(10):
-    print(random.randint(1,10))
-    
-    
-print("good ckbcdjhbdjhdj morning")    
-for i in range(10):
-    print(random.randint(1,10))
