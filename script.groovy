54d1dbf4545567eabf14690d9cb5b204877e7b8b
ShekharRedd <shekharreddy4558@gmail.com>
2023-07-03 14:36:16 +0530

    adding file

--- script.groovy
+++ script.groovy
@@ -1,17 +1,17 @@
 def build(){
     echo "hello"
-    sh "cd D:"
+    sh "cd C:"
     sh "ls"
-    sh "cd gitbash"
-    sh "cd gitsecond"
-    sh "cd jenkinsgit"
-    sh "git switch ram"
-    sh "git status"
-    sh "git pull"
-    sh "git add ."
-    sh 'git commit -m "adding file"'
-    sh "git push"
-    echo "hello"
+    // sh "cd gitbash"
+    // sh "cd gitsecond"
+    // sh "cd jenkinsgit"
+    // sh "git switch ram"
+    // sh "git status"
+    // sh "git pull"
+    // sh "git add ."
+    // sh 'git commit -m "adding file"'
+    // sh "git push"
+    // echo "hello"
     
 }
 
